ecda01f8c5d18668c22fca682ade269373e2999d
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:24:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi1;
+Console.WriteLine("Bir sayı giriniz : ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+if (0 < sayi1 && sayi1 < 100)
+{
+    Console.WriteLine("Geçerli sayı ");
+}
+else
+{
+    Console.WriteLine("Geçerli sayı değil ");
+}
